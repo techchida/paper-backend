b7c825824f4f05aba6f9dcef49d9ab8564a509b1
Henry chidebe <techchida@outlook.com>
2023-10-25 15:17:51 +0100

    fix cors bug

--- config/cors.js
+++ config/cors.js
@@ -2,18 +2,12 @@ require("dotenv").config();
 
 const whitelist = {
   dev: ["http://localhost:5173", "undefined"],
-  prod: ["https://invoicepaper.vercel.app"],
+  prod: ["https://invoicepaper.vercel.app", "http://localhost:5173"],
 };
 
 const verifyOrigin = (ctx) => {
   // Get requesting origin hostname
-  var origin = ctx.headers.origin;
-  // Make sure it's a valid origin
-  if (whitelist[process.env.NODE_ENV].indexOf(origin) != -1) {
-    // Set the header to the requested origin
-    ctx.set("Access-Control-Allow-Origin", origin);
-    return origin;
-  }
+  return ctx.headers.origin;
 };
 
 module.exports = {
